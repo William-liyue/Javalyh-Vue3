1271ef680d2e9057f07a5d28b5d5055236cc0144
William Li <1927457049@qq.com>
2023-01-21 22:34:52 +0800

    Initial commit

---
+++ src/layout/footer/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    Copyright © 2022-2023 Javalyh 版权所有&nbsp;&nbsp;<a href="http://www.javalyh.com" target="_blank">www.javalyh.com</a>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.footer{
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>
